4c2f8b67eb02a23fef67100af9a05ab7d5ab4194
Dustin Myers <dustint314@gmail.com>
2021-02-09 11:52:07 -0700

    wip

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,2 +1,7 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+
+// set up a class component that has state for doggos
+// fetch data from "https://dog.ceo/api/breed/husky/images"
+// set that data to state when it returns
+// render the data in the render function
