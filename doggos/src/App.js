08e02f8068b604ae639e0dacb78c1c5cdce3c85e
Dustin Myers <dustint314@gmail.com>
2021-02-09 11:53:41 -0700

    wip

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -5,3 +5,25 @@ import axios from "axios";
 // fetch data from "https://dog.ceo/api/breed/husky/images"
 // set that data to state when it returns
 // render the data in the render function
+
+class App extends React.Component {
+  state = {
+    doggos: [],
+  };
+
+  render() {
+    console.log("rendering");
+    return (
+      <div className="App">
+        <h1>Hello Doggos</h1>
+        <div className="doggos">
+          {this.state.doggos.map((doggo) => (
+            <img width="200" src={doggo} key={doggo} alt={doggo} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
