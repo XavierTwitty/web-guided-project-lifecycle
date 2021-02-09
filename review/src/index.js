7f31b64a835ac9593bedc6feb6b1d4c376885a20
Dustin Myers <dustint314@gmail.com>
2021-02-09 10:15:43 -0700

    wip

--- review/src/index.js
+++ review/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.scss';
-import AppFunc from './AppFunc';
-import AppClass from './AppClass';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.scss";
+import AppFunc from "./AppFunc";
+import AppClass from "./AppClass";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<AppFunc />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(<AppClass />, rootElement);
